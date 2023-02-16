cc87b9501683814fe4dd33b556c8060ea232ea92
Roberto <robertorw7@gmail.com>
2023-02-15 22:00:27 -0700

    first commit

---
+++ app/src/main/java/potros/itson/practica4/MenuActivity.kt
@@ -0,0 +1,11 @@
+package potros.itson.practica4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+}
